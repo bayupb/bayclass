e163bc4ad69b922bd7614137451bc0fbe385d3cc
bayupb <kampuspb.bayu@gmail.com>
2022-05-05 16:12:03 +0700

    master

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode:['jit'],
+  content: [
+    '*'
+  ],
+  theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+      'quicksand': ['Quicksand', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
